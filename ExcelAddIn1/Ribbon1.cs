02c881c86c964ac64271799e8628fa6d3473b1d2
mathm07008 <alexandros@betonera.com>
2016-12-15 15:00:16 +0200

    as

--- ExcelAddIn1/Ribbon1.cs
+++ ExcelAddIn1/Ribbon1.cs
@@ -6,6 +6,8 @@ using Microsoft.Office.Tools.Ribbon;
 using MySql.Data.MySqlClient;
 using System.Data;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Timers;
 
 namespace ExcelAddIn1
 {
@@ -15,13 +17,21 @@ namespace ExcelAddIn1
         MySqlConnection con;
         DataGridView fixturesGrid;
         excelTraderForm mainForm = new excelTraderForm();
-        int fixCode;
-        int runner;
+        int oddCode;        
+        Microsoft.Office.Interop.Excel.Workbook objBook;        
+        Microsoft.Office.Interop.Excel._Worksheet workSheet;
+        System.Timers.Timer aTimer = new System.Timers.Timer();
+
         private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
         {
             con = c.getConnection();
             con.Open();
             mainForm.button1.Click += new System.EventHandler(this.set_Click);
+            mainForm.StartButton.Click += new System.EventHandler(this.start_Click);
+            mainForm.StopButton.Click += new System.EventHandler(this.stop_Click);
+            mainForm.StopButton.Enabled = false;
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 7000;
         }
 
         private void button1_Click(object sender, RibbonControlEventArgs e)
@@ -34,19 +44,17 @@ namespace ExcelAddIn1
             
             fixturesGrid = mainForm.dataGridView1;
 
-            string c1 = @"SELECT A.FixCode as Code, A.SportName as Sport, A.CompetitionName as Competition, 
-                    A.HomeTeamName as HomeTeam, A.AwayTeamName as AwayTeam, A.StartsTime 
-                    FROM vOddsPivoted AS A 
-                    INNER JOIN Competitions as B on A.Competition=B.Code 
-                    INNER JOIN Sports as C on A.Sport=C.Code 
-                    WHERE A.StartsTime >= now() and B.IsSelected=1 and C.IsSelected=1 and A.Bookmaker=1 
-                    ORDER BY A.StartsTime ASC";
+            string c1 = @"SELECT 
+                Code, HomeTeamName as HomeTeam, AwayTeamName as AwayTeam, TypeName as Type, RunnerType as Runner, BackPrice, StartsTime
+                FROM vOdds
+                WHERE StartsTime > now() AND StartsTime< (now() + INTERVAL 3 Day) AND Bookmaker = 1
+                ORDER BY StartsTime ASC;";
 
             MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(c1, con);
-            DataTable DT = new DataTable();
+            System.Data.DataTable DT = new System.Data.DataTable();
             mySqlDataAdapter.Fill(DT);
-            DataTable tempDT = new DataTable();
-            tempDT = DT.DefaultView.ToTable(true, "Code", "Sport", "Competition", "HomeTeam", "AwayTeam", "StartsTime");
+            System.Data.DataTable tempDT = new System.Data.DataTable();
+            tempDT = DT.DefaultView.ToTable(true, "Code", "HomeTeam", "AwayTeam", "Type", "Runner", "BackPrice", "StartsTime");
             fixturesGrid.DataSource = tempDT;
             fixturesGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(fixturesGrid_SelectionChanged);
             fixturesGrid.DataBindingComplete += (o, _) =>
@@ -67,54 +75,97 @@ namespace ExcelAddIn1
             foreach (DataGridViewRow row in fixturesGrid.SelectedRows)
             {
                 int cd = Int32.Parse(row.Cells[0].Value.ToString());
-                fixCode = cd;
-                
+                oddCode = cd;                
             }
         } 
 
-        private int setSelectedRunner()
-        {
-            int run = 0;  
-            //var buttons = mainForm.Controls.OfType<RadioButton>()                           .FirstOrDefault(n => n.Checked);
-            if (mainForm.homeRadio.Checked)
-            {
-                run = 1;
-            }
-            if (mainForm.awayRadio.Checked)
-            {
-                run = 2;
-            }
-            if (mainForm.drawRadio.Checked)
-            {
-                run = 3;
-            }
-            runner = run;
-            return run;
-        }
-
-        private int selectOddCode(int runner)
+        
+        private double getBackPrice(int oddCode)
         {            
-            int oc=0;
-            string selectQuery = @"SELECT Code FROM vOdds WHERE FixCode=@fixCode AND Runner=@runner AND Bookmaker=1 AND Type=1";
+            double oc = 0;
+            string selectQuery = @"SELECT BackPrice FROM vOdds WHERE Code=@oddCode";
             MySqlCommand cmd = new MySqlCommand(selectQuery, con);
-            cmd.Parameters.Add("@fixCode", MySqlDbType.Int32);
-            cmd.Parameters["@fixCode"].Value = fixCode;
-            cmd.Parameters.Add("@runner", MySqlDbType.Int32);
-            cmd.Parameters["@runner"].Value = runner;
+            cmd.Parameters.Add("@oddCode", MySqlDbType.Int32);
+            cmd.Parameters["@oddCode"].Value = oddCode;            
             MySqlDataReader reader = cmd.ExecuteReader();
             if (reader.Read())                
             {
-                oc = Int32.Parse(reader[0].ToString());
+                oc = reader.GetDouble(0);
             }
             reader.Close();
             return oc;
         }
 
-        
+        private double getHistogram(int oddCode)
+        {
+            double oc = 0;
+            string selectQuery = @"SELECT Histogramm FROM vMacd 
+                WHERE OddCode=@oddCode 
+                AND InsertDate=(SELECT max(InsertDate) FROM vMacd where OddCode=@oddCode) ";
+            MySqlCommand cmd = new MySqlCommand(selectQuery, con);
+            cmd.Parameters.Add("@oddCode", MySqlDbType.Int32);
+            cmd.Parameters["@oddCode"].Value = oddCode;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                if (!reader.IsDBNull(0))
+                {
+                    oc = reader.GetDouble(0);
+                } else
+                {
+                    oc = -100000;
+                }                                
+            }
+            reader.Close();
+            return oc;
+        }
+
+        private void setExcelCell(string col, int row, object value)
+        {
+            objBook = (Workbook)Globals.ThisAddIn.Application.ActiveWorkbook;
+            workSheet = (Worksheet)objBook.ActiveSheet;
+            workSheet.Cells[row, col] = value;            
+        }
         private void set_Click(object sender, EventArgs e)
         {
-            int oc = selectOddCode(setSelectedRunner());            
-            mainForm.oddCodeTb.Text = oc.ToString();
+             setExcelValues();
+
+        }
+
+        private void setExcelValues()
+        {
+            if ((!string.IsNullOrWhiteSpace(mainForm.oddlColTb.Text)) && (!string.IsNullOrWhiteSpace(mainForm.oddRowTb.Text))
+                  && (!string.IsNullOrWhiteSpace(mainForm.histColTb.Text)) && (!string.IsNullOrWhiteSpace(mainForm.histRowTb.Text)))
+            {
+                string a1 = mainForm.oddlColTb.Text;
+                string b1 = mainForm.histColTb.Text;
+                int a2 = Int32.Parse(mainForm.oddRowTb.Text);
+                int b2 = Int32.Parse(mainForm.histRowTb.Text);
+                setExcelCell(a1, a2, getBackPrice(oddCode));
+                setExcelCell(b1, b2, getHistogram(oddCode));
+            }
+            else
+            {
+                MessageBox.Show("Fill cell properties malaka");
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            aTimer.Enabled = true;
+            mainForm.StartButton.Enabled = false;
+            mainForm.StopButton.Enabled = true;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            aTimer.Enabled = false;
+            mainForm.StartButton.Enabled = true;
+            mainForm.StopButton.Enabled = false;
+        }
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            setExcelValues();
         }
     }
 }
--- ExcelAddIn1/excelTraderForm.Designer.cs
+++ ExcelAddIn1/excelTraderForm.Designer.cs
@@ -31,14 +31,22 @@ namespace ExcelAddIn1
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.homeRadio = new System.Windows.Forms.RadioButton();
-            this.awayRadio = new System.Windows.Forms.RadioButton();
-            this.drawRadio = new System.Windows.Forms.RadioButton();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.oddCodeTb = new System.Windows.Forms.TextBox();
+            this.oddlColTb = new System.Windows.Forms.TextBox();
+            this.oddRowTb = new System.Windows.Forms.TextBox();
+            this.excelCellLb = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.histColTb = new System.Windows.Forms.TextBox();
+            this.histRowTb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -54,87 +62,142 @@ namespace ExcelAddIn1
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(997, 286);
+            this.dataGridView1.Size = new System.Drawing.Size(824, 365);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // homeRadio
-            // 
-            this.homeRadio.AutoSize = true;
-            this.homeRadio.Location = new System.Drawing.Point(6, 19);
-            this.homeRadio.Name = "homeRadio";
-            this.homeRadio.Size = new System.Drawing.Size(53, 17);
-            this.homeRadio.TabIndex = 4;
-            this.homeRadio.TabStop = true;
-            this.homeRadio.Text = "Home";
-            this.homeRadio.UseVisualStyleBackColor = true;
-            // 
-            // awayRadio
-            // 
-            this.awayRadio.AutoSize = true;
-            this.awayRadio.Location = new System.Drawing.Point(6, 40);
-            this.awayRadio.Name = "awayRadio";
-            this.awayRadio.Size = new System.Drawing.Size(51, 17);
-            this.awayRadio.TabIndex = 5;
-            this.awayRadio.TabStop = true;
-            this.awayRadio.Text = "Away";
-            this.awayRadio.UseVisualStyleBackColor = true;
-            // 
-            // drawRadio
-            // 
-            this.drawRadio.AutoSize = true;
-            this.drawRadio.Location = new System.Drawing.Point(6, 61);
-            this.drawRadio.Name = "drawRadio";
-            this.drawRadio.Size = new System.Drawing.Size(50, 17);
-            this.drawRadio.TabIndex = 6;
-            this.drawRadio.TabStop = true;
-            this.drawRadio.Text = "Draw";
-            this.drawRadio.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.homeRadio);
-            this.groupBox1.Controls.Add(this.drawRadio);
-            this.groupBox1.Controls.Add(this.awayRadio);
-            this.groupBox1.Location = new System.Drawing.Point(12, 292);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(73, 93);
-            this.groupBox1.TabIndex = 7;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Runners";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(910, 353);
+            this.button1.Location = new System.Drawing.Point(130, 28);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(57, 23);
             this.button1.TabIndex = 8;
             this.button1.Text = "Set";
             this.button1.UseVisualStyleBackColor = true;
-            
             // 
-            // oddCodeTb
+            // oddlColTb
             // 
-            this.oddCodeTb.Location = new System.Drawing.Point(137, 311);
-            this.oddCodeTb.Name = "oddCodeTb";
-            this.oddCodeTb.Size = new System.Drawing.Size(100, 20);
-            this.oddCodeTb.TabIndex = 9;
+            this.oddlColTb.Location = new System.Drawing.Point(52, 30);
+            this.oddlColTb.Name = "oddlColTb";
+            this.oddlColTb.Size = new System.Drawing.Size(29, 20);
+            this.oddlColTb.TabIndex = 9;
+            // 
+            // oddRowTb
+            // 
+            this.oddRowTb.Location = new System.Drawing.Point(83, 30);
+            this.oddRowTb.Name = "oddRowTb";
+            this.oddRowTb.Size = new System.Drawing.Size(30, 20);
+            this.oddRowTb.TabIndex = 10;
+            // 
+            // excelCellLb
+            // 
+            this.excelCellLb.AutoSize = true;
+            this.excelCellLb.Location = new System.Drawing.Point(6, 33);
+            this.excelCellLb.Name = "excelCellLb";
+            this.excelCellLb.Size = new System.Drawing.Size(27, 13);
+            this.excelCellLb.TabIndex = 11;
+            this.excelCellLb.Text = "Odd";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Histog";
+            // 
+            // histColTb
+            // 
+            this.histColTb.Location = new System.Drawing.Point(52, 54);
+            this.histColTb.Name = "histColTb";
+            this.histColTb.Size = new System.Drawing.Size(29, 20);
+            this.histColTb.TabIndex = 13;
+            // 
+            // histRowTb
+            // 
+            this.histRowTb.Location = new System.Drawing.Point(83, 54);
+            this.histRowTb.Name = "histRowTb";
+            this.histRowTb.Size = new System.Drawing.Size(30, 20);
+            this.histRowTb.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Col";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Row";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.histRowTb);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.oddRowTb);
+            this.groupBox1.Controls.Add(this.oddlColTb);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.histColTb);
+            this.groupBox1.Controls.Add(this.excelCellLb);
+            this.groupBox1.Location = new System.Drawing.Point(12, 371);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 89);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(19, 20);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(57, 23);
+            this.StartButton.TabIndex = 17;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(19, 51);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(57, 23);
+            this.StopButton.TabIndex = 18;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.StartButton);
+            this.groupBox2.Controls.Add(this.StopButton);
+            this.groupBox2.Location = new System.Drawing.Point(720, 371);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(92, 89);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
             // 
             // excelTraderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(997, 397);
-            this.Controls.Add(this.oddCodeTb);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(824, 472);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "excelTraderForm";
-            this.Text = "excelTrader";
+            this.Text = "Excel Trader";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -143,11 +206,18 @@ namespace ExcelAddIn1
         
 
         public System.Windows.Forms.DataGridView dataGridView1;
-        public RadioButton homeRadio;
-        public RadioButton awayRadio;
-        public RadioButton drawRadio;
-        public GroupBox groupBox1;
         public Button button1;
-        public TextBox oddCodeTb;
+        public TextBox oddlColTb;
+        public TextBox oddRowTb;
+        private Label excelCellLb;
+        private Label label1;
+        public TextBox histColTb;
+        public TextBox histRowTb;
+        private Label label2;
+        private Label label3;
+        private GroupBox groupBox1;
+        public Button StartButton;
+        public Button StopButton;
+        private GroupBox groupBox2;
     }
 }
